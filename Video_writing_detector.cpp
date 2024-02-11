faa6ff3fc9e5a1f651c481b09d34008796720824
Avinash S <117369858+Avinashs7@users.noreply.github.com>
2024-02-11 10:20:09 +0530

    Update Video_writing_detector.cpp
    Documented

--- Video_writing_detector.cpp
+++ Video_writing_detector.cpp
@@ -10,44 +10,73 @@ int main(int argc,char** argv) {
     // If the input is the web camera, pass 0 instead of the video file name
     VideoCapture cap;
     if (argc >= 2) {
-        //
+        //arg[1] is the path of video file to be processed, it needs to passed in command line
         cap=VideoCapture(argv[1]);
     }
-    else {// this else statement is for the exe to run on our local machine only
+    else {
+        // this else statement is for the exe to run on our local machine only
         cap=VideoCapture ("C:\\Users\\CS\\Desktop\\Hackathon.mp4");
     }
     // Check if camera opened successfully
     if (!cap.isOpened()) {
+        //handling the error, if it fail to load video file from specified path
         cout << "Error opening video stream or file" << endl;
         return -1;
     }
-    int width = cap.get(CAP_PROP_FRAME_WIDTH);
-    int height = cap.get(CAP_PROP_FRAME_HEIGHT);
-    VideoWriter video("output.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 10, Size(width, height), true);
+    int width = cap.get(CAP_PROP_FRAME_WIDTH);        //frame width of video file
+    int height = cap.get(CAP_PROP_FRAME_HEIGHT);      //frame height of video file
+    
+    //initialising video writer with encoding type XVID and specifying the path of output file but over here only file name with extension is specified hence stores in current directory
+    VideoWriter video("output.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 10, Size(width, height), true);   
+    
     uchar extra_initial_conflict = 0b1111, initial_conflict_frames = 10;
+
+    //Declaring Mat objects necessary for processing
     Mat img, bg,conflict, temp1, temp2, gray, kern ,frame;
+
+    // img is the 3 channel frame of size of video's frame
     img = Mat(height,width,CV_8UC3);
+
+    //temp1,temp2,gray,bg are 1 channel converted of size img
     temp1 = Mat(img.rows, img.cols, CV_8UC1);
     temp2 = Mat(img.rows, img.cols, CV_8UC1);
-    frame = Mat(img.rows, img.cols, CV_8UC3);
     gray = Mat(img.rows, img.cols, CV_8UC1);
     bg = Mat(img.rows, img.cols, CV_8UC1);
     conflict = Mat(img.rows, img.cols, CV_8UC1);
+
+    //frame is 3 channel copy of img
+    frame = Mat(img.rows, img.cols, CV_8UC3);
+
+    //kernal of single channel of size 5,5
     kern = Mat::ones(Size(5, 5), CV_8UC1);
+
+    //variable to check there is a background frame or not
     bool first_frame = true;
+    
     while (1) {
 
         // Capture frame-by-frame
         if (!cap.read(img))
             break;
+
+        //applying gaussian blur for reducing noise in the frame
         GaussianBlur(img, frame, Size(5, 5), 0);
+
+        //converting it to gray image copy to Mat object gray
         cvtColor(frame, gray, COLOR_BGR2GRAY);
+        
         // If the frame is empty, break immediately
         if (first_frame) {
-            first_frame = false;
+            //if there is no background frame then assign current frame as background and start processing
+            //this if block runs only once, that is start of processing a video after that anyway we will store the current frame to previous frame 
+            first_frame = false;    
             gray.copyTo(bg);
         }
+        
+        //it takes the absolute difference of previous and current frame and store it in temp1
         absdiff(gray, bg, temp1);
+        
+        //If the difference is between 20 to 255, then considereed as significant change orelse ignored
         inRange(temp1, 20, 255, temp2);
 
         if (initial_conflict_frames > 0) {
@@ -74,6 +103,7 @@ int main(int argc,char** argv) {
         inRange(temp1, 128, 255, temp2);
         filter2D(temp2, temp1, -1, kern);
 
+        //Identifying the sharp edges using canny detector
         Canny(temp1, temp2, 0, 255);
 
         for (int x = 0; x < bg.rows; x++) {
@@ -87,6 +117,7 @@ int main(int argc,char** argv) {
             }
         }
 
+        //writing the overlayed img frame to the video writer stream
         video.write(img);
 
     }
